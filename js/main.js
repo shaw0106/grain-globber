6a5b2f6cbedbd7fd0bea91cafbb78232436aaf3b
Shannon <shaw0106@algonquinlive.com>
2016-03-18 08:43:59 -0400

    won't show

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 grains.forEach(function (item) {
   var $ul = $('ul');
   var $li = $('<li>');
+  var $img = $('<img>');
+  var $p = $('<p>');
+  var $h2 = $('<h2>');
+
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+});
